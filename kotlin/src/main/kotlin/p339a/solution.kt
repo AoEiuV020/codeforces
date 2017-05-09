13aeb72b3ccd5e8157b8d3d10963e3a244f4324c
AoEiuV020 <aoeiuv020@gmail.com>
2017-05-09 20:27:49 +0800

    Soluted 339a

---
+++ kotlin/src/main/kotlin/p339a/solution.kt
@@ -0,0 +1,10 @@
+package p339a;
+
+import java.util.*
+
+/**
+ * Created by AoEiuV020 on 2017/05/09.
+ */
+fun main(vararg args: String) {
+    print(readLine()!!.split('+').sorted().joinToString("+"))
+}
