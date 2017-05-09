7fef6cdc5177a34e614f530ce493739e7a9f4bff
AoEiuV020 <aoeiuv020@gmail.com>
2017-05-09 14:18:25 +0800

    允许多次测试

--- kotlin/src/test/kotlin/base/BaseTest.kt
+++ kotlin/src/test/kotlin/base/BaseTest.kt
@@ -6,13 +6,13 @@ import java.io.*
  * Created by AoEiuV020 on 2017/05/08.
  */
 open class BaseTest {
-    private val buffer = ByteArrayOutputStream()
-    val output by lazy { buffer.toByteArray().inputStream().reader().readText() }
-    init {
-        System.setOut(PrintStream(buffer))
-    }
+    private lateinit var buffer: ByteArrayOutputStream
+    val output 
+        get() = buffer.toByteArray().inputStream().reader().readText()
     fun input(str: String) {
         System.setIn(str.trimIndent().byteInputStream())
+        buffer = ByteArrayOutputStream()
+        System.setOut(PrintStream(buffer))
     }
 }
 
