5725c841cedcad0efab62d15109d824ce79c491f
AoEiuV020 <aoeiuv020@gmail.com>
2017-05-09 18:50:37 +0800

    Fix a warning
    一个没用到的scanner删除了，

--- kotlin/src/main/kotlin/p231a/solution.kt
+++ kotlin/src/main/kotlin/p231a/solution.kt
@@ -6,7 +6,6 @@ import java.util.*
  * Created by AoEiuV020 on 2017/05/09.
  */
 fun main(vararg args: String) {
-    val scanner = Scanner(System.`in`)
     val n = readLine()!!.toInt()
     var count = 0
     for (i in 1..n) {
