c2d2f362ce9bfca5c0d24868c54c669c1d8345c0
AoEiuV020 <aoeiuv020@gmail.com>
2021-01-24 14:50:31 +0800

    accepted 1471a
    使用math.ceil简化上取整操作，

--- python/p1471a/main.py
+++ python/p1471a/main.py
@@ -1,5 +1,6 @@
 # https://codeforces.com/problemset/problem/1471/A
+import math
 for _ in range(int(input())):
     n, x = map(int, input().split())
     arr = list(map(int, input().split()))
-    print(-(sum(arr) // -x), sum(-(k // -x) for k in arr))
+    print(math.ceil(sum(arr) / x), sum(math.ceil(k / x) for k in arr))
